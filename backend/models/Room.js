7205d8eb7039ac88394284cf0f439f1fcc77af70
Ken <kenlee2002@outlook.com>
2025-07-27 15:43:27 +0900

    fix:Remove is_direct2

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -15,11 +15,6 @@ const Room = sequelize.define('Room', {
     type: DataTypes.TEXT,
     allowNull: true
   },
-  is_direct: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-    comment: '1:1 채팅방 여부'
-  },
   created_at: {
     type: DataTypes.DATE,
     defaultValue: DataTypes.NOW
